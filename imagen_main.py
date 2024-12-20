312431d1f7db15b8752d6c74aca8971684355098
Aacatoc <aavatoc@gmail.com>
2024-12-20 11:17:20 +0000

    Chore: Update image generation logic and enhance task handling

--- imagen_main.py
+++ imagen_main.py
@@ -1,6 +1,8 @@
 import concurrent.futures
 from copy import deepcopy
+import datetime
 import io
+import random
 import subprocess
 import concurrent
 from uuid import uuid4
@@ -13,45 +15,45 @@ prepare_environment()
 
 from modules.async_worker import ImageTaskProcessor
 import ldm_patched.modules.model_management
-from h3_utils.config import ImageGenerationObject, OverWriteControls, DefaultConfigImageGen
+from h3_utils.config import LAUNCH_ARGS, ImageGenerationObject, OverWriteControls, DefaultConfigImageGen, HooocusConfig
 from unavoided_globals.global_model_management import global_model_management
 from PIL import Image
 from h3_utils.logging_util import LoggingUtil
 log = LoggingUtil().get_logger()
 import time
 
-imgProcessor = ImageTaskProcessor()
+
 
 class BatchTemplates:
-    _shared = DefaultConfigImageGen
+    _shared = HooocusConfig
     _shared.image_number = 1
 
     normal = ImageGenerationObject(
-        cfg_scale=15.0,
-        sample_sharpness=15.0,
         )
 
 
+imgProcessor = ImageTaskProcessor()
 
 def generate_image_to_stream(prompt: str):
     # https://stackoverflow.com/questions/65971081/streaming-video-from-camera-in-fastapi-results-in-frozen-image-after-first-frame
     unique_id = uuid4().hex
     newtask = deepcopy(BatchTemplates.normal)
+    newtask.seed = random.randint(LAUNCH_ARGS.min_seed, LAUNCH_ARGS.max_seed)
     newtask.uid = unique_id
     newtask.prompt = prompt
+    newtask.performance_selection = Performance.LIGHTNING
     imgProcessor.generation_tasks.append(newtask)
 
     # Create and run task asynchronously
     concurrent.futures.ThreadPoolExecutor().submit(imgProcessor.process_all_tasks)
     
     finished = False
-    max_loops = 100
+    max_loops = 1000
     while not finished:
-        if max_loops == 0:
-            log.error('Max loops reached.')
-            break
         max_loops -= 1
-        time.sleep(1.01)
+        if max_loops <= 0:
+            raise Exception('Max loops reached.')
+        time.sleep(1.0)
         if len(imgProcessor.yields) > 0:
             try:
                 img_res = imgProcessor.yields.pop(0)
@@ -79,41 +81,29 @@ def generate_image_to_stream(prompt: str):
                 log.debug('No image preview generated.')
                 continue
 
+def get_filename_from_image() -> str:
+    dt_string = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    return f"outputs/{dt_string}.jpg"
                 
 
+def generate_image(prompt: str, savesteps: bool = False, image_filename = None) -> bool:
+    # https://stackoverflow.com/questions/65971081/streaming-video-from-camera-in-fastapi-results-in-frozen-image-after-first-frame
+    unique_id = uuid4().hex
+    newtask = deepcopy(BatchTemplates.normal)
+    newtask.seed = random.randint(LAUNCH_ARGS.min_seed, LAUNCH_ARGS.max_seed)
+    newtask.uid = unique_id
+    newtask.prompt = prompt
+    newtask.performance_selection = Performance.LIGHTNING
+    imgProcessor.generation_tasks.append(newtask)
+
+    # Create and run task asynchronously
+    concurrent.futures.ThreadPoolExecutor().submit(imgProcessor.process_all_tasks)
+    
 
 
-def generate_image(task: ImageGenerationObject):
-
-    try:
-        with global_model_management.interrupt_processing_mutex:
-            global_model_management.interrupt_processing = False
-        
-            print(task.refiner_model)
-            imgProcessor = ImageTaskProcessor()
-            imgProcessor.generation_tasks.append(task)
-            finished = False
-
-            time.sleep(0.01)
-            imgProcessor.process_all_tasks()
-            print('Image generation finished.')
-            ...
-
-    except Exception as e:
-        print(str(e))
-
-    finally:
-        print('Done')
-
-overwrites = OverWriteControls(overwrite_step=15)
 
 if __name__ == '__main__':
-    image_params = ImageGenerationObject(
-        prompt="A car without headlights a banana",
-        performance_selection=Performance.SPEED,
-        overwrite_controls=overwrites,
-        )
+    # overwrites = OverWriteControls(overwrite_step=15)
+    prompt = 'a cat in the forest, at night oil painting'
     
-    generate_image(image_params)
-    
-    print('Done fom main.py')
+    generate_image(prompt, savesteps=True)
