63fafb1774f24e28d240dafa68d6f85ddd909f88
Aacatoc <aavatoc@gmail.com>
2024-12-20 11:16:55 +0000

    Chore: Add root endpoint and improve error handling for prompt processing

--- server/main.py
+++ server/main.py
@@ -9,7 +9,7 @@ ROOT_DIR = os.path.abspath(__file__).split("server")[0]
 sys.path.append(ROOT_DIR)
 
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi.responses import JSONResponse, StreamingResponse
 from imagen_main import generate_image_to_stream
 import uvicorn
 from PIL import Image
@@ -17,14 +17,20 @@ from PIL import Image
 app = FastAPI()
 
 
-
+@app.get("/")
+def read_root():
+    return JSONResponse(content="Hello World", status_code=200)
     
 
 @app.get("/getphoto/{prompt}")
 def main(prompt: str):
-    prompt = prompt.replace("_", " ")
+    try:
+        prompt = prompt.replace("_", " ")
+    except Exception as e:
+        return JSONResponse(content=str(e), status_code=500)
     return StreamingResponse(generate_image_to_stream(prompt), media_type="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == "__main__":
     uvicorn.run(app)
+    pass
