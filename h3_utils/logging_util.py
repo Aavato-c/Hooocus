a9e74ff9a8957c8520cdd9ff3fee56a5c9959332
Aacatoc <aavatoc@gmail.com>
2025-01-02 15:11:52 +0000

    Chore: Update logging format to include line number and change log file name to combined_2.log

--- h3_utils/logging_util.py
+++ h3_utils/logging_util.py
@@ -14,8 +14,9 @@ class LoggingUtil:
         else:
             self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        file_handler = logging.FileHandler('logs/combined.log')
+        #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(lineno)d')
+        file_handler = logging.FileHandler('logs/combined_2.log')
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
