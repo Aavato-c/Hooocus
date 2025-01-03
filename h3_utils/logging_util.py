f7ab4c825bfd288a80a34a028b2c0ee53273745c
Aavato C <aavatoc@gmail.com>
2025-01-04 00:38:42 +0100

    Refactor: logging utility

--- h3_utils/logging_util.py
+++ h3_utils/logging_util.py
@@ -7,19 +7,22 @@ if not os.path.exists('logs'):
     os.makedirs('logs')
     
 class LoggingUtil:
-    def __init__(self, name: str = None, dont_overwrite_progress_bar: bool = True):
+    def __init__(self, name: str = None):
         if not name:
-            random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
-            self.logger = logging.getLogger(random_chars)
+            name = "_"
+            self.logger = logging.getLogger(name)
+            if os.path.exists('logs/combined_2.log'):
+                os.remove('logs/combined_2.log')
         else:
             self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.INFO)
+        self.logger.propagate = False # Prevents double logging
+        self.logger.setLevel(logging.DEBUG)
         #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(lineno)d')
-        file_handler = logging.FileHandler('logs/combined_2.log')
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
+        formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(module)s:%(lineno)d:\t%(message)s', datefmt='%m%d:%H:%M:%S')
+        #file_handler = logging.FileHandler('logs/combined_2.log')
+        #file_handler.setLevel(logging.DEBUG)
+        #file_handler.setFormatter(formatter)
+        #self.logger.addHandler(file_handler)
 
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
@@ -27,11 +30,6 @@ class LoggingUtil:
         self.logger.addHandler(stream_handler)
         
 
-        if dont_overwrite_progress_bar != True:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setFormatter(formatter)
-            stream_handler.setLevel(logging.DEBUG)
-            self.logger.addHandler(stream_handler)
 
 
 
