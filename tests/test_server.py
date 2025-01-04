4473902c4ddff97a871e8eb3d388a0ae384b15ee
Aavato C <aavatoc@gmail.com>
2025-01-04 11:53:04 +0100

    Refactor: Update tests to match the new server

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,34 +1,52 @@
-import os, sys
+import os, sys, dotenv, pytest
+from requests import Session as RequestsSession
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(CURR_DIR.split("tests")[0])
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__).split("tests")[0])))
-
-# These variables are used in the auth/auth_handlers.py:verify_user function
-
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # To solve the problem of importing modules from different directories
-os.sys.path.insert(0,parentdir) 
-
-from requests import Session
-import dotenv
-
-from fastapi import Response
-
+from h3_utils.config import ImageGenerationObject
 from h3_utils.logging_util import LoggingUtil
 
+from tests.utils_for_testing import test_client_main
+
+import db.models as dbm
+
 dotenv.load_dotenv()
+
+ACCEPTED_API_TOKEN_FOR_IMAGE_GENERATION = os.environ.get("ACCEPTED_API_TOKEN_FOR_IMAGE_GENERATION")
+
 log = LoggingUtil(name="TESTING").get_logger()
 
+class DataManager:
+    client = test_client_main
 
-session = Session()
+    def __init__(self):
+        self.test_token = ACCEPTED_API_TOKEN_FOR_IMAGE_GENERATION
+        self.headers_for_auth = {"Authorization": f"Bearer {self.test_token}"}
+        self.client.headers.update(self.headers_for_auth)
+
+@pytest.fixture(name="data_manager", scope="module")
+def data_manager_fixture() -> DataManager:
+    return DataManager()
+
+
+def test_setups_for_tests(self, data_manager: DataManager):
+    new_image_order = ImageGenerationObject()
+    new_image_order.prompt = "Cat oil painting sun moon smoke classical museum portrait painting oil on canvas rembrandt"
+    response = data_manager.client.post("/getphoto", data=new_image_order.model_dump_json())
+    assert response.status_code == 200
+    
 
 
 def test_getphoto():
-    response = session.get("http://127.0.0.1:8000/getphoto/this_is_a_test_cat_oil_painting_sun_moon", stream=True)
+    #response = session.get("http://127.0.0.1:8000/getphoto/this_is_a_test_cat_oil_painting_sun_moon", stream=True)
+    response = test_client_main.get("/this_is_a_test_cat_oil_painting_sun_moon.jpg", allow_redirects=True)
     if response.status_code == 200:
-        for chunk in response.iter_content(chunk_size=8192):
+        """ for chunk in response.iter_content(chunk_size=8192):
             log.info(f"Got stream bit of size {len(chunk)}")
-            log.info(f"Chunk: ...{chunk[-10:]}")
+            log.info(f"Chunk: ...{chunk[-10:]}") """
+        pass
     else:
         log.error(f"Failed to get photo: {response.status_code}")
 
 if __name__ == "__main__":
-    test_getphoto()
+    pytest.main([__file__, "-W", "ignore::pytest.PytestAssertRewriteWarning"])
--- tests/utils_for_testing.py
+++ tests/utils_for_testing.py
@@ -1,25 +1,24 @@
+import os, sys
 import io
 import random
-import os
-
-import random
-import re
 import time
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, CURR_DIR.split("tests")[0])
+
 import PIL
 import PIL.Image
 import dotenv
 
 from uuid import uuid4
 
-from typing import Generator
-
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # To solve the problem of importing modules from different directories
-os.sys.path.insert(0,parentdir) 
-
 from fastapi.testclient import TestClient
-from fastapi.websockets import WebSocket
-from httpx import AsyncClient
-from starlette.websockets import WebSocketDisconnect
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.decl_api import DeclarativeBase
+
+import db.models.sqlalchemy_m as dbm
+from db.database import get_db
 
 from h3_utils.logging_util import LoggingUtil
 
@@ -27,13 +26,40 @@ from server.main import app
 
 dotenv.load_dotenv()
 
+log = LoggingUtil("Test_db_setup").get_logger()
 
+DB_URL_TEST = os.environ.get("DB_URL_TEST")
+
+Base: DeclarativeBase = dbm.Base
+
+""" if FORMAT_TEST_DATABASE:
+    try:
+        master_engine = create_engine(DB_URL_MASTER).execution_options(isolation_level="AUTOCOMMIT")
+        conn = master_engine.connect()
+        log.info(f"Dropping test database {DB_NAME_TEST}")
+        conn.execute(text(f'DROP DATABASE IF EXISTS {DB_NAME_TEST} WITH (FORCE)'))
+        log.info(f"Creating test database {DB_NAME_TEST}")
+        conn.execute(text(f'CREATE DATABASE {DB_NAME_TEST}'))
+    finally:
+        conn.close()
+        master_engine.dispose()
+        master_engine.clear_compiled_cache()
+        time.sleep(10) """
+
+main_engine = create_engine(DB_URL_TEST)
+Base.metadata.create_all(bind=main_engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=main_engine)
+
+def get_test_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = get_test_db
 test_client_main = TestClient(app)
 test_client_secondary = TestClient(app)
-
-async def async_client():
-    async with AsyncClient(app=app) as client:
-        yield client
 
 def get_random_string(prefix: str = None, length: int = 3):
     letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
