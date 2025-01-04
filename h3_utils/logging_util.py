d7469acfb9555f098393807446b69d9dd08cb547
Aavato C <aavatoc@gmail.com>
2025-01-04 19:15:13 +0100

    Refactor: Enhance LoggingUtil to conditionally log to file based on parameter

--- h3_utils/logging_util.py
+++ h3_utils/logging_util.py
@@ -7,7 +7,8 @@ if not os.path.exists('logs'):
     os.makedirs('logs')
     
 class LoggingUtil:
-    def __init__(self, name: str = None):
+    def __init__(self, name: str = None, log_to_file: bool = False):
+        self.log_to_file = log_to_file
         if not name:
             name = "_"
             self.logger = logging.getLogger(name)
@@ -15,14 +16,15 @@ class LoggingUtil:
                 os.remove('logs/combined_2.log')
         else:
             self.logger = logging.getLogger(name)
-        self.logger.propagate = False # Prevents double logging
+
+        # self.logger.propagate = False
         self.logger.setLevel(logging.INFO)
-        #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(module)s:%(lineno)d:\t%(message)s', datefmt='%m%d:%H:%M:%S')
-        #file_handler = logging.FileHandler('logs/combined_2.log')
-        #file_handler.setLevel(logging.DEBUG)
-        #file_handler.setFormatter(formatter)
-        #self.logger.addHandler(file_handler)
+        if self.log_to_file:
+            file_handler = logging.FileHandler('logs/combined_2.log')
+            file_handler.setLevel(logging.DEBUG)
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
 
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
