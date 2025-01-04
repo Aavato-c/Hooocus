00b43395df6d8c438823fe80b6e971c34fbf4ccc
Aavato C <aavatoc@gmail.com>
2025-01-04 11:02:35 +0100

    Refactor: Enhance user verification by raising HTTP exceptions for invalid tokens

--- server/auth_handlers.py
+++ server/auth_handlers.py
@@ -5,7 +5,7 @@ sys.path.insert(0, CURR_DIR.split("server")[0])
 from h3_utils.logging_util import LoggingUtil
 
 from fastapi.security import OAuth2
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 
 from typing import Annotated
 
@@ -18,13 +18,13 @@ log = logger.get_logger()
 
 oauth2_scheme = OAuth2()
 
-async def verify_user(token: Annotated[str, Depends(oauth2_scheme)]) -> bool:
+async def verify_user(token: Annotated[str, Depends(oauth2_scheme)]):
     try:
             token = token.replace("Bearer ", "")
             if token != ACCEPTED_API_TOKEN_FOR_IMAGE_GENERATION:
-                return False
+                raise HTTPException(status_code=401, detail="")
             return True
     except Exception as e:
         log.error(f"Error verifying user: {e}")
-        return False
+        raise HTTPException(status_code=401, detail="")
 
