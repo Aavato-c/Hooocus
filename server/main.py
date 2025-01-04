c9198a61dd37932f4f1a0e76437f039be3717d1a
Aavato C <aavatoc@gmail.com>
2025-01-04 11:54:07 +0100

    Refactor: Update photo serving logic to use UUID and improve error handling

--- server/main.py
+++ server/main.py
@@ -5,16 +5,18 @@ from typing import Annotated
 from uuid import uuid4
 import uvicorn
 
+from h3_utils.path_configs import FolderPathsConfig
 from server.auth_handlers import verify_user
 ROOT_DIR = os.path.abspath(__file__).split("server")[0]
 sys.path.append(ROOT_DIR)
 
 from sqlalchemy.orm import Session
 
-from fastapi import FastAPI, Response, Depends
-from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi import FastAPI, HTTPException, Response, Depends
+from fastapi.responses import JSONResponse, RedirectResponse, StreamingResponse
 
 from db.database import get_db
+from db import crud
 
 from imagen_main import generate_image_to_stream
 
@@ -42,33 +44,44 @@ if SERVER_URL is None:
 def read_root():
     return JSONResponse(content="Hello World", status_code=200)
 
-@app.get("/{filename}.{extension}")
-def serve_photo(filename: str, extension: str):
+@app.get("/{file_uuid}.{extension}")
+def serve_photo(file_uuid: str, extension: str, db: Session = Depends(get_db)):
     try:
-        if os.path.exists(f"outputs/{filename}.{extension}") is False:
-            return JSONResponse(content="File not found.", status_code=404)
-        with open(f"outputs/{filename}.{extension}", "rb") as f:
-            photo = f.read()
-            return Response(content=photo, media_type=f"image/{extension}")
+        gen_status = crud.should_generate_or_url(file_uuid)
+        match gen_status:
+            
+            case "generate":
+                order_data = crud.get_imageorder(db, file_uuid)
+                if order_data is None:
+                    return JSONResponse(content="", status_code=404)
+                StreamingResponse(generate_image_to_stream(order_data, file_uuid), media_type="multipart/x-mixed-replace; boundary=frame")
+            
+            case "url":
+                if os.path.exists(f"{FolderPathsConfig.path_outputs}/{file_uuid}.{extension}"):
+                    with open(f"outputs/{file_uuid}.{extension}", "rb") as f:
+                        photo = f.read()
+                    return Response(content=photo, media_type=f"image/{extension}")
+                else:
+                    raise HTTPException(status_code=500)
+            
+            case "not_found":
+                return JSONResponse(status_code=404)
         
     except Exception as e:
-        return JSONResponse(content=str(e), status_code=500)
+        log.error(f"Error serving photo: {e}")
+        return JSONResponse(status_code=500)
     
-@app.post("/getphoto")
+@app.post("/gen/photo/normal")
 def get_photo_genobject(_is_verified: Annotated[bool, Depends(verify_user)], request: dict, db: Session = Depends(get_db)):
     try:
-        new_id = uuid4().hex
         request_validated = ImageGenerationObject.model_validate(request)
-        prompt = request_validated.prompt
-        # Todo handle input image urls here
+        uuid_of_order = crud.add_imageorder(db, request_validated)
+        return RedirectResponse(url=f"{SERVER_URL}/{uuid_of_order}.webp")
     except Exception as e:
-        return JSONResponse(content=str(e), status_code=500)
-    try:
-        return StreamingResponse(generate_image_to_stream(request_validated, new_id), media_type="multipart/x-mixed-replace; boundary=frame")
-    except Exception as e:
-        return JSONResponse(content=str(e), status_code=500)
-    finally:
-        pass
+        log.error(f"Error adding image order: {e}")
+        return JSONResponse(status_code=500)
+
+
 
 def main_entry(process_uuid = None, max_processes = 1):
     if process_uuid == None:
