a2375a5c2ca663b492e471d66de8e2ceec3a0916
Aavato C <aavatoc@gmail.com>
2025-01-05 11:43:48 +0100

    Refactor: Update logging utility to use RotatingFileHandler and improve log file management

--- h3_utils/logging_util.py
+++ h3_utils/logging_util.py
@@ -2,27 +2,28 @@ import logging
 import os
 import random
 import string
-
+from logging.handlers import RotatingFileHandler
 if not os.path.exists('logs'):
     os.makedirs('logs')
-    
+
+
 class LoggingUtil:
     def __init__(self, name: str = None, log_to_file: bool = False):
         self.log_to_file = log_to_file
         if not name:
             name = "_"
             self.logger = logging.getLogger(name)
-            if os.path.exists('logs/combined_2.log'):
-                os.remove('logs/combined_2.log')
+            if os.path.exists('logs/combined.log'):
+                os.remove('logs/combined.log')
         else:
             self.logger = logging.getLogger(name)
 
-        # self.logger.propagate = False
         self.logger.setLevel(logging.INFO)
         formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(module)s:%(lineno)d:\t%(message)s', datefmt='%m%d:%H:%M:%S')
+        
         if self.log_to_file:
-            file_handler = logging.FileHandler('logs/combined_2.log')
-            file_handler.setLevel(logging.DEBUG)
+            file_handler = RotatingFileHandler('logs/combined.log', maxBytes=5_000_000, backupCount=3) # 5MB
+            file_handler.setLevel(logging.INFO)
             file_handler.setFormatter(formatter)
             self.logger.addHandler(file_handler)
 
@@ -30,10 +31,6 @@ class LoggingUtil:
         stream_handler.setFormatter(formatter)
         stream_handler.setLevel(logging.INFO)
         self.logger.addHandler(stream_handler)
-        
-
-
-
 
     def get_logger(self):
         return self.logger
