17991b5b0831b97f3f4a876f86acc76d5d4acb68
Aavato C <aavatoc@gmail.com>
2025-01-08 14:42:13 +0100

    Feat: Enhance logging functionality with JSON serialization and error handling

--- h3_utils/config.py
+++ h3_utils/config.py
@@ -360,7 +360,7 @@ class ImageGenerationObject(_InitialImageGenerationParams):
         if len(self.additional_style_lamdas) > 0:
             self.styles.extend(self.additional_style_lamdas)
 
-    def save_log(self):
+    def save_log_json(self):
         while os.path.exists(f"{PARENT_DIR}/logs/imagen_logs/{self.uid}.json"):
             self.uid = f"{self.uid}_1"
         
@@ -369,6 +369,33 @@ class ImageGenerationObject(_InitialImageGenerationParams):
                 self.performance_selection = self.performance_selection.name
                 json_model = self.model_dump()
                 json.dump(json_model, f, indent=4, ensure_ascii=False)
+        except Exception as e:
+            if "serialization" in str(e):
+                log.error("Could not serialize model.")
+
+                for key, value in self.dict().items():
+                    try:
+                        json.dumps({key: value})
+                    except Exception as e:
+                        log.error(f"Could not serialize {key} with value {value}.")
+                        # Delete the key
+                        self.__delattr__(key)
+                try:
+                    self.save_log()
+                except Exception as e:
+                    log.error("Could not save log.")
+            else:
+                log.error(f"Could not save log: {e}")
+
+    def save_log(self):
+        try:
+            self.performance_selection = self.performance_selection.name
+            json_model = self.model_dump_json()
+            if crud.update_imageorder_log(self.uid, json_model):
+                log.info(f"Saved log for {self.uid}.")
+            else:
+                log.error(f"Could not save log for {self.uid}.")
+                log.error(f"Model: {json_model}")
         except Exception as e:
             if "serialization" in str(e):
                 log.error("Could not serialize model.")
