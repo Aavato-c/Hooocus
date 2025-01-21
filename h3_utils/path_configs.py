3b551f7e2d124d6150f7411c2d2832e41f5975bf
Aavato C <aavatoc@gmail.com>
2025-01-21 12:14:41 +0100

    Refactor: Update folder paths for models and styles

--- h3_utils/path_configs.py
+++ h3_utils/path_configs.py
@@ -11,6 +11,7 @@ from h3_utils.logging_util import LoggingUtil
 log = LoggingUtil(__name__).get_logger()
 
 class FolderPathsConfig:
+    path_facexlib = "./models/facexlib/"
     path_checkpoints = "./models/checkpoints/"
     path_loras = "./models/loras/"
     path_embeddings = "./models/embeddings/"
@@ -21,15 +22,16 @@ class FolderPathsConfig:
     path_controlnet = "./models/controlnet/"
     path_clip_vision = "./models/clip_vision/"
     path_fooocus_expansion = "./models/prompt_expansion/fooocus_expansion"
-    path_wildcards = "./h3_utils/sdxl_styles/wildcards/"
+    path_wildcards = "./modules/sdxl_styles/wildcards/"
     path_safety_checker = "./models/safety_checker/"
     path_sam = "./models/sam/"
-    path_prompt_style_samples = "./h3_utils/sdxl_styles/samples/"
+    path_prompt_style_samples = "./modules/sdxl_styles/samples/"
     
     default_temp_path = os.path.join(tempfile.gettempdir(), 'hooocus')
     path_outputs: str = "outputs" # Don't use a dot here
 
 class _EnumFolderPaths(Enum):
+    path_facexlib = "./models/facexlib/"
     path_checkpoints = "./models/checkpoints/"
     path_loras = "./models/loras/"
     path_embeddings = "./models/embeddings/"
@@ -40,10 +42,10 @@ class _EnumFolderPaths(Enum):
     path_controlnet = "./models/controlnet/"
     path_clip_vision = "./models/clip_vision/"
     path_fooocus_expansion = "./models/prompt_expansion/fooocus_expansion"
-    path_wildcards = "./h3_utils/sdxl_styles/wildcards/"
+    path_wildcards = "./modules/sdxl_styles/wildcards/"
     path_safety_checker = "./models/safety_checker/"
     path_sam = "./models/sam/"
-    path_prompt_style_samples = "./h3_utils/sdxl_styles/samples/"
+    path_prompt_style_samples = "./modules/sdxl_styles/samples/"
     
     default_temp_path = os.path.join(tempfile.gettempdir(), 'hooocus')
     path_outputs: str = "./outputs"
