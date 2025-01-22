c6be9c8e27c61a361c85a2678f813927d949f1aa
Aavato C <aavatoc@gmail.com>
2025-01-22 11:38:29 +0100

    Visual: Lint crud.py

--- db/crud.py
+++ db/crud.py
@@ -1,4 +1,5 @@
 import os, sys
+
 CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, CURR_DIR.split("db")[0])
 
@@ -27,21 +28,27 @@ log = LoggingUtil(__name__).get_logger()
 # =============================================================================
 #    Image order functions
 # =============================================================================
-def add_imageorder(db: Session, order_data: ImageGenerationObjectForRequests, optional_uuid: UUIDType = None) -> UUIDType:
+def add_imageorder(
+    db: Session,
+    order_data: ImageGenerationObjectForRequests,
+    optional_uuid: UUIDType = None,
+) -> UUIDType:
     """Add a new image order to the database
 
     Args:
         db (Session): SQLAlchemy Session
         order_data (pm.ImageOrderInCreate): Image order data
-        
+
     Returns:
         uuid_of_new_order (UUID): The ID of the new image order
     """
     try:
-        log.debug(f"Adding new image order. Order uid: {order_data.uid}. Imagetask seed: {order_data.seed}")
+        log.debug(
+            f"Adding new image order. Order uid: {order_data.uid}. Imagetask seed: {order_data.seed}"
+        )
         new_order = pm.ImageOrderInCreate(generation_data=order_data.model_dump_json())
         new_order_to_add = sm.ImageOrder(**new_order.model_dump())
-        
+
         if optional_uuid is not None:
             log.warning(f"Optional UUID provided: {optional_uuid}")
             new_order_to_add.id = optional_uuid
@@ -58,7 +65,10 @@ def add_imageorder(db: Session, order_data: ImageGenerationObjectForRequests, op
         log.error(f"Error adding event: {e}")
         raise e
 
-def update_imageorder_status(db: Session, order_id: UUIDType, status: pm.GenerationStates.Lit, uri: str = None) -> bool:
+
+def update_imageorder_status(
+    db: Session, order_id: UUIDType, status: pm.GenerationStates.Lit, uri: str = None
+) -> bool:
     """Update the status of an image order
 
     Args:
@@ -66,9 +76,9 @@ def update_imageorder_status(db: Session, order_id: UUIDType, status: pm.Generat
         order_id (UUID): The ID of the image order
         status (pm.GenerationStates.Lit): The status of the image order
         uri (Optional[str], None): The URI of the image, default is None
-        
+
     Returns:
-        bool: True if successful 
+        bool: True if successful
 
     Raises:
         Exception: If an error occurs
@@ -84,14 +94,16 @@ def update_imageorder_status(db: Session, order_id: UUIDType, status: pm.Generat
         # Can be removed later
         try:
             # Check if the order has been updated
-            updated_order = db.query(sm.ImageOrder).filter(sm.ImageOrder.id == order_id).first()
+            updated_order = (
+                db.query(sm.ImageOrder).filter(sm.ImageOrder.id == order_id).first()
+            )
             if updated_order.generation_state != status:
                 raise Exception("Failed to update image order status")
         except Exception as e:
             log.error(f"Error updating image order status: {e}")
             raise e
         # =====================================================================
-            
+
         return True
     except Exception as e:
         log.error(f"Error updating image order status: {e}")
@@ -104,7 +116,7 @@ def get_imageorder(db: Session, order_id: UUIDType) -> pm.ImageOrderInResponse:
     Args:
         db (Session): SQLAlchemy Session
         order_id (UUID): The ID of the image order
-        
+
     Returns:
         pm.ImageOrderInResponse: The image order
 
@@ -117,7 +129,8 @@ def get_imageorder(db: Session, order_id: UUIDType) -> pm.ImageOrderInResponse:
         return pm.ImageOrderInResponse.model_validate(order)
     except Exception as e:
         log.error(f"Error getting image order: {e}")
-        raise e    
+        raise e
+
 
 def should_generate_or_url(db: Session, order_id: UUIDType) -> pm.GenerationStates.Lit:
     """Check if an image order should be generated or if the URL should be returned
@@ -125,7 +138,7 @@ def should_generate_or_url(db: Session, order_id: UUIDType) -> pm.GenerationStat
     Args:
         db (Session): SQLAlchemy Session
         order_id (UUID): The ID of the image order
-        
+
     Returns:
         Literal["generate", "url", "not_found"]: "generate" if the image should be generated, "url" if the URL should be returned, "not_found" if the image order is not found
 
@@ -133,7 +146,11 @@ def should_generate_or_url(db: Session, order_id: UUIDType) -> pm.GenerationStat
         Exception: If an error occurs
     """
     try:
-        generation_state = db.query(sm.ImageOrder.generation_state).filter(sm.ImageOrder.id == order_id).first()
+        generation_state = (
+            db.query(sm.ImageOrder.generation_state)
+            .filter(sm.ImageOrder.id == order_id)
+            .first()
+        )
         if generation_state is None:
             return "not_found"
         return generation_state[0]
@@ -142,30 +159,44 @@ def should_generate_or_url(db: Session, order_id: UUIDType) -> pm.GenerationStat
         raise e
 
 
-def update_inmem_img_cache(db: Session, order_id: UUIDType, img_data: bytearray, img_format: str) -> bool:
+def update_inmem_img_cache(
+    db: Session, order_id: UUIDType, img_data: bytearray, img_format: str
+) -> bool:
     log.debug(f"Updating in-memory image cache for order: {order_id}")
     try:
         start_time = perf_counter()
-        data_to_add = pm.TempImgDataAdd(order_uuid=order_id, image_format=img_format, image_data=img_data, updated_at=get_timestamp())
+        data_to_add = pm.TempImgDataAdd(
+            order_uuid=order_id,
+            image_format=img_format,
+            image_data=img_data,
+            updated_at=get_timestamp(),
+        )
         log.debug(f"Adding temp image data with updated_at: {data_to_add.updated_at}")
         data_to_add = sm.TempImgData(**data_to_add.model_dump())
         log.debug(data_to_add.updated_at)
         db.add(data_to_add)
         db.commit()
-        log.debug(f"Time taken to update in-memory image cache: {perf_counter() - start_time}")
+        log.debug(
+            f"Time taken to update in-memory image cache: {perf_counter() - start_time}"
+        )
         return True
     except Exception as e:
         log.error(f"Error updating in-memory image cache: {e}")
         raise e
 
-def get_temp_img_for_order(inmem_db: Session, order_id: UUIDType, finished: bool = False) -> Union[bytearray, str] | None:
+
+def get_temp_img_for_order(
+    inmem_db: Session, order_id: UUIDType, finished: bool = False
+) -> Union[bytearray, str] | None:
     try:
         start_time = perf_counter()
-        if finished: 
+        if finished:
             nofile = True
             for outputformat in OutputFormat.all:
                 try:
-                    with open(f"{OUTPUT_DIR}/{order_id}.{outputformat}", "rb") as f: #TOOD Handle format change
+                    with open(
+                        f"{OUTPUT_DIR}/{order_id}.{outputformat}", "rb"
+                    ) as f:  # TOOD Handle format change
                         img_data = f.read()
                         img_format = outputformat
                         nofile = False
@@ -176,7 +207,7 @@ def get_temp_img_for_order(inmem_db: Session, order_id: UUIDType, finished: bool
                 raise FileNotFoundError(f"File not found for order: {order_id}")
             return img_data, img_format
 
-        else:            
+        else:
             img_data, img_format, updated_at = (
                 inmem_db.query(
                     sm.TempImgData.image_data,
@@ -187,27 +218,41 @@ def get_temp_img_for_order(inmem_db: Session, order_id: UUIDType, finished: bool
                 .order_by(sm.TempImgData.updated_at.desc())
                 .first()
             )
-            log.debug(f"Image update time: {datetime.datetime.fromtimestamp(updated_at)}")
+            log.debug(
+                f"Image update time: {datetime.datetime.fromtimestamp(updated_at)}"
+            )
             if img_data is None:
                 return None
-            log.debug(f"Time taken to get in-memory image cache: {perf_counter() - start_time}")
+            log.debug(
+                f"Time taken to get in-memory image cache: {perf_counter() - start_time}"
+            )
             return img_data, img_format
     except Exception as e:
         log.error(f"Error getting in-memory image cache: {e}")
         raise e
+
 
 def clear_cache_for_temp_img(db: Session, order_id: UUIDType | str) -> bool:
     try:
         start_time = perf_counter()
         db.query(sm.TempImgData).filter(sm.TempImgData.order_uuid == order_id).delete()
         db.commit()
-        log.debug(f"Time taken to clear in-memory image cache: {perf_counter() - start_time}")
+        log.debug(
+            f"Time taken to clear in-memory image cache: {perf_counter() - start_time}"
+        )
         return True
     except Exception as e:
         log.error(f"Error clearing in-memory image cache: {e}")
         raise e
 
-def add_process(pid: int, guni_uid: str, max_processes: int, process_name: str = "H3_proc", process_metadata: dict = {}) -> UUIDType:
+
+def add_process(
+    pid: int,
+    guni_uid: str,
+    max_processes: int,
+    process_name: str = "H3_proc",
+    process_metadata: dict = {},
+) -> UUIDType:
     try:
         try:
             db = get_db_unmanaged()
@@ -216,19 +261,18 @@ def add_process(pid: int, guni_uid: str, max_processes: int, process_name: str =
                 gunicorn_uid=guni_uid,
                 max_processes=max_processes,
                 process_name=process_name,
-                process_metadata=process_metadata
+                process_metadata=process_metadata,
             )
             new_process_to_add = sm.Process(**new_process.model_dump())
             db.add(new_process_to_add)
             db.commit()
         finally:
             db.close()
-        
+
         return new_process.id
     except Exception as e:
         log.error(f"Error adding process: {e}")
         raise e
-    
 
 
 if __name__ == "__main__":
